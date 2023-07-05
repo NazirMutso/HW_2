286bcb50a7a69d5a0052521b08f897a425234061
Nazir <135230714+NazirMutso@users.noreply.github.com>
2023-07-05 20:55:35 +0300

    Разработал main функцию.

---
+++ main.py
@@ -0,0 +1,11 @@
+from table_maker import Vacancies_pars, Vacancies_api, db_name
+from funcs import get_links, get_values, get_vacancies_api, get_values_api, pars_master_2000
+
+url = 'https://hh.ru/search/vacancy?page='
+url_api = 'https://api.hh.ru/vacancies?page='
+
+# парсинг страниц
+pars_master_2000(url, db_name, Vacancies_pars, get_links, get_values)
+
+# парсинг API
+pars_master_2000(url_api, db_name, Vacancies_api, get_vacancies_api, get_values_api)
