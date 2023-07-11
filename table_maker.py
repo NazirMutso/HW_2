7c3fde5c790afca6338a5d38b00c9693bdff919c
Nazir <135230714+NazirMutso@users.noreply.github.com>
2023-07-11 23:15:04 +0300

    Добавил столбец ID вакансии

--- table_maker.py
+++ table_maker.py
@@ -11,24 +11,26 @@ class Base(DeclarativeBase):
 
 class Vacancies_pars(Base):
     __tablename__ = table_name
-    company_names: Mapped[str] = mapped_column(primary_key=True)
+    vacancy_id: Mapped[int] = mapped_column(primary_key=True)
+    company_names: Mapped[str]
     position: Mapped[str]
     job_description: Mapped[str]
     key_skills: Mapped[str]
 
     def __repr__(self):
-        return f'{self.company_names}, {self.position}, {self.job_description}, {self.key_skills}'
+        return f'{self.vacancy_id}, {self.company_names}, {self.position}, {self.job_description}, {self.key_skills}'
 
 
 class Vacancies_api(Base):
     __tablename__ = table_name2
-    company_names: Mapped[str] = mapped_column(primary_key=True)
+    vacancy_id: Mapped[int] = mapped_column(primary_key=True)
+    company_names: Mapped[str]
     position: Mapped[str]
     job_description: Mapped[str]
     key_skills: Mapped[str]
 
     def __repr__(self):
-        return f'{self.company_names}, {self.position}, {self.job_description}, {self.key_skills}'
+        return f'{self.vacancy_id}, {self.company_names}, {self.position}, {self.job_description}, {self.key_skills}'
 
 
 Base.metadata.create_all(engine)
